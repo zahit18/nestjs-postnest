726d9a351d97baf92c0272b5912bb9c636b758ad
zahit18 <zahithdez@gmail.com>
2025-05-27 21:26:25 -0700

    Connect with DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule} from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: typeOrmConfig,
+      inject: [ConfigService]
     }),
     CategoriesModule],
   controllers: [AppController],
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,5 +1,12 @@
+import { ConfigService } from '@nestjs/config'
 import type { TypeOrmModuleOptions } from '@nestjs/typeorm'
 
-export const typeOrmConfig = () : TypeOrmModuleOptions => ({
-    type: 'postgres'
+export const typeOrmConfig = (configService: ConfigService): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: configService.get('DATABASE_HOST'),
+    port: configService.get('DATABASE_PORT'),
+    username: configService.get('DATABASE_USER'),
+    password: configService.get('DATABASE_PASS'),
+    database: configService.get('DATABASE_NAME'),
+    ssl: true
 })
