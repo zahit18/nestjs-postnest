b0f300ee059a7c64dfbed168ee031fec2c799c8e
zahit18 <zahithdez@gmail.com>
2025-06-07 10:53:09 -0700

    Add Product Entity & Relations with Category

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -1,10 +1,14 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Product } from "src/products/entities/product.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Category {
     @PrimaryGeneratedColumn()
     id: number
 
-    @Column({type: 'varchar', length: 60})
+    @Column({ type: 'varchar', length: 60 })
     name: string
+
+    @OneToMany(() => Product, (product) => product.category, { cascade: true })
+    products: Product[]
 }
