425355ba8d772064bb9f5c9436ef183e423818fc
zahit18 <zahithdez@gmail.com>
2025-06-07 14:38:42 -0700

    Transactions Entities

---
+++ src/transactions/entities/transaction.entity.ts
@@ -0,0 +1,35 @@
+import { Product } from "src/products/entities/product.entity";
+import { Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Transaction {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column('decimal')
+    total: number
+
+    @Column({ type: 'timestamp', default: () => "CURRENT_TIMESTAMP(6)" })
+    transactionDate: Date
+
+    @OneToMany(() => TransactionContents, (transaction) => transaction.transaction)
+    contents: TransactionContents[]
+}
+
+@Entity()
+export class TransactionContents {
+    @PrimaryGeneratedColumn()
+    id: number
+
+    @Column('int')
+    quantity: number
+
+    @Column('decimal')
+    price: number
+
+    @ManyToOne(() => Product, (product) => product.id, { eager: true })
+    product: Product
+
+    @ManyToOne(() => Transaction, (transaction) => transaction.contents)
+    transaction: Transaction
+}
