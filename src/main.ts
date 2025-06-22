b497f90b1fb2273691a454ce5a47480b23224030
zahit18 <zahithdez@gmail.com>
2025-06-21 17:20:32 -0700

    Add public assets in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true
   }));
+  app.useStaticAssets(join(__dirname, '../public'))
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
