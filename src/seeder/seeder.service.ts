f3bbf6d4f352117dc64cc45a28b943a2e262a576
zahit18 <zahithdez@gmail.com>
2025-06-21 17:16:24 -0700

    Create seed

--- src/seeder/seeder.service.ts
+++ src/seeder/seeder.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { Category } from '../categories/entities/category.entity';
+import { Product } from '../products/entities/product.entity';
+import { categories } from './data/categories';
+import { products } from './data/products';
 
 @Injectable()
 export class SeederService {
+    constructor(
+        @InjectRepository(Category) private readonly categoryRepository: Repository<Category>,
+        @InjectRepository(Product) private readonly productRepository: Repository<Product>,
+        private dataSourcer: DataSource
+    ) { }
+
+    async onModuleInit() {
+        const connection = this.dataSourcer
+        await connection.dropDatabase()
+        await connection.synchronize()
+    }
+
     async seed() {
-        console.log('desde seed')
+        await this.categoryRepository.save(categories)
+        for await (const seedProduct of products) {
+            const category = await this.categoryRepository.findOneBy({ id: seedProduct.categoryId })
+            if (category) {
+                const product = new Product()
+                product.name = seedProduct.name
+                product.imgage = seedProduct.image
+                product.price = seedProduct.price
+                product.inventory = seedProduct.inventory
+                product.category = category
+
+                await this.productRepository.save(product)
+            }
+        }
     }
 }
